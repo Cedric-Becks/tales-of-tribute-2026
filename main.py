637b82bdbb46c5af9eabfab0df8e4bdede6ec7be
Cedric Becks <cedric.becks@gmail.com>
2026-06-03 01:38:54 +0200

    Added option to launch bot on port 50000

--- main.py
+++ main.py
@@ -3,20 +3,35 @@ from heuristics import max_prestige, max_patrons
 from scripts_of_tribute.game import Game
 from bot import ISMCTSBot
 
-def main():
-    player_one = ISMCTSBot("prestigemaxxer", MCTSNode, max_prestige)
-    player_two = ISMCTSBot("patronmaxxer", MCTSNode, max_patrons)
+BASE_CLIENT_PORT=50000
+BASE_SERVER_PORT=49000
+THREADS=1
+RUNS=10
+
+def main(run_game: bool):
     game = Game()
-    game.register_bot(player_one)
-    game.register_bot(player_two)
+    if run_game:
+        player_one = ISMCTSBot("prestigemaxxer", MCTSNode, max_prestige)
+        player_two = ISMCTSBot("patronmaxxer", MCTSNode, max_patrons)
+        game.register_bot(player_one)
+        game.register_bot(player_two)
     
-    game.run(
-        "prestigemaxxer",
-        "patronmaxxer",
-        start_game_runner=True,
-        runs=10,
-        threads=10,
-    )
+        game.run(
+            "prestigemaxxer",
+            "patronmaxxer",
+            start_game_runner=True,
+            runs=RUNS,
+            threads=THREADS,
+        )
+    else:
+        game._run_bot_instances(
+            ISMCTSBot("prestigemaxxer", MCTSNode, max_prestige),
+            ISMCTSBot("patronmaxxer", MCTSNode, max_patrons),
+            num_threads=1,
+            base_client_port=BASE_CLIENT_PORT,
+            base_server_port=BASE_SERVER_PORT)
+        while True:
+            pass
 
 if __name__ == "__main__":
-    main()
+    main(False)
