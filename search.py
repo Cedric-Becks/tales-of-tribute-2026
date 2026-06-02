6653e9bc0436f6f968d1385c8a5ab23c92326719
Cedric Becks <cedric.becks@gmail.com>
2026-06-02 20:46:28 +0200

    Implemented basic MCTS

---
+++ search.py
@@ -0,0 +1,84 @@
+
+from __future__ import annotations
+from typing import Optional
+from typing import Callable
+from typing import List
+from typing import Tuple
+
+from random import randint
+from cmath import log, sqrt, inf
+from abc import ABC, abstractmethod
+
+from scripts_of_tribute.move import BasicMove
+from scripts_of_tribute.enums import MoveEnum
+from scripts_of_tribute.board import GameState
+
+
+class Search(ABC):
+    children: dict[BasicMove, Optional[Search]] = {}
+    visits: int = 0
+    fullyExpanded: bool = False
+    maxScore: float = inf
+    minScore: float = -inf
+    score: int = 0
+    leaf: bool = False
+
+    gameState: GameState
+    
+    @abstractmethod
+    def __init__(self, gameState: GameState, moves: List[BasicMove]):
+        pass
+
+    @abstractmethod
+    def simulate(self, heuristic: Callable[[GameState], int]) -> Tuple[int, int]:
+        pass
+
+    @abstractmethod
+    def expand(self, move: BasicMove) -> None:
+        pass
+
+    def ucbScore(self, simulations: int) -> complex | float:
+        if self.visits == 0:
+            return self.maxScore
+        if self.fullyExpanded:
+            return self.minScore
+        return self.score + 1.41 * sqrt(log(simulations)/self.visits)
+
+
+
+class MCTSNode(Search):
+    realMoves: List[BasicMove]
+
+    def __init__(self, gameState: GameState, moves: List[BasicMove]):
+        self.gameState = gameState
+        if moves is None or len(moves) <= 1:
+            self.moves = moves
+            self.leaf = True
+            self.fullyExpanded = True
+        else:
+            self.children = {move: None for move in moves}
+            self.moves = [x for x in moves if x.command != MoveEnum.END_TURN]
+
+    def expand(self, move: BasicMove) -> None:
+        if move.command == MoveEnum.END_TURN:
+            self.children[move] = MCTSNode(self.gameState, [move])
+        else:
+            game_state, moves = self.gameState.apply_move(move)
+            self.children[move] = MCTSNode(game_state, moves)
+
+    def simulate(self, heuristic: Callable[[GameState], int]) -> Tuple[int, int]:
+        if self.leaf:
+            return heuristic(self.gameState), 0
+        moveCount = 0
+        moves: list[BasicMove] = self.realMoves
+        move: BasicMove = moves[randint(0,len(moves))]
+        state: GameState = self.gameState
+        while (move.command != MoveEnum.END_TURN):
+            moveCount += 1
+            state, moves = state.apply_move(move)
+            moves = [x for x in moves if x.command != MoveEnum.END_TURN]
+            if (len(moves) > 0):
+                move = moves[randint(0,len(moves))]
+            else:
+                break
+        return heuristic(state), moveCount
